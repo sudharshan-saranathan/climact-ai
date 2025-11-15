e24fed9d721ed79b293d1028c469db2ad09fb472
sudharshan-saranathan <sudharshan.saranathan@outlook.in>
2025-11-15 20:27:45 +0530

    Work in progress...

--- apps/config/stream_config.py
+++ apps/config/stream_config.py
@@ -4,98 +4,15 @@
 # Description: Configuration utility for resources.
 # ----------------------------------------------------------------------------------------------------------------------
 
+from typing import List, Type, Dict, Any
+
 # Import(s):
 # PySide6:
-from PySide6 import QtCore
+from PySide6 import QtCore, QtGui
 from PySide6 import QtWidgets
 
 # Qtawesome
 import qtawesome as qta
-
-PhysicalStreams = {
-    'Mass'       :  {
-        'unit' : 'kg',
-        'icon' : 'mdi.weight-gram',
-        'color': 'lightblue'
-    },
-
-    'Flow'       :  {
-        'unit' : 'kg/s',
-        'icon' : 'mdi.speedometer',
-        'color': '#00aced'
-    },
-
-    'Energy'     :  {
-        'unit' : 'kJ',
-        'icon' : 'mdi.thermometer',
-        'color': '#ffcb00'
-    },
-
-    'Electricity':  {
-        'unit' : 'kW',
-        'icon' : 'mdi.flash',
-        'color': '#8c2b76'
-    },
-}
-
-EmissionStreams = {
-    'Carbon'     :  {
-        'unit' : 'kg CO2-eq/kg',
-        'icon' : 'mdi.alpha-c',
-        'color': '#efefef'
-    },
-
-    'Sulphur'    :  {
-        'unit' : 'kg SO2-eq/kg',
-        'icon' : 'mdi.alpha-s',
-        'color': '#efefef'
-    },
-
-    'Nitrogen'   :  {
-        'unit' : 'kg NOx-eq/kg',
-        'icon' : 'mdi.alpha-n',
-        'color': '#efefef'
-    },
-
-    'PM 2.5'     :  {
-        'unit' : 'kg PM2.5/kg',
-        'icon' : 'mdi.circle-small',
-        'color': '#efefef'
-    },
-
-    'PM 10'      :  {
-        'unit' : 'kg PM10/kg',
-        'icon' : 'mdi.circle-medium',
-        'color': '#efefef'
-    },
-}
-
-EconomicStreams = {
-
-    'CapEx'     : {
-        'unit' : 'INR/year',
-        'icon' : 'mdi.cash',
-        'color': 'lightgreen'
-    },
-
-    'OprEx'     :  {
-        'unit' : 'INR/year',
-        'icon' : 'mdi.account-cog',
-        'color': 'red'
-    },
-
-    'Labour'    : {
-        'unit' : '#',
-        'icon' : 'mdi.account-multiple',
-        'color': '#ffcb00'
-    },
-
-    'Land'     : {
-        'unit' : 'sq.m',
-        'icon' : 'mdi.island',
-        'color': 'brown'
-    }
-}
 
 # Class StreamConfigWidget: A tree widget for managing resource types.
 class StreamConfigWidget(QtWidgets.QTreeWidget):
@@ -113,31 +30,18 @@ class StreamConfigWidget(QtWidgets.QTreeWidget):
         self.setColumnWidth(0, 150)
 
         # Create top-level items:
-        phy_root = QtWidgets.QTreeWidgetItem(self); phy_root.setText(0, 'Physical')
-        eco_root = QtWidgets.QTreeWidgetItem(self); eco_root.setText(0, 'Economics')
-        ghg_root = QtWidgets.QTreeWidgetItem(self); ghg_root.setText(0, 'Emissions')
-        new_root = QtWidgets.QTreeWidgetItem(self); new_root.setText(0, 'Custom')
+        self._phy_root = QtWidgets.QTreeWidgetItem(self); self._phy_root.setText(0, 'Physical')
+        self._raw_root = QtWidgets.QTreeWidgetItem(self); self._raw_root.setText(0, 'Material')
+        self._fue_root = QtWidgets.QTreeWidgetItem(self); self._fue_root.setText(0, 'Fuel')
+        self._eco_root = QtWidgets.QTreeWidgetItem(self); self._eco_root.setText(0, 'Economic')
+        self._ghg_root = QtWidgets.QTreeWidgetItem(self); self._ghg_root.setText(0, 'Emission')
+        self._new_root = QtWidgets.QTreeWidgetItem(self); self._new_root.setText(0, 'Custom')
 
-        for key, value in PhysicalStreams.items():
-            item = QtWidgets.QTreeWidgetItem(phy_root, [key, ''])
-            item.setText(0, key)
-            item.setText(1, value['unit'])
-            item.setIcon(0, qta.icon(value['icon'], color=value['color']))
-
-        for key, value in EmissionStreams.items():
-            item = QtWidgets.QTreeWidgetItem(ghg_root, [key, ''])
-            item.setText(0, key)
-            item.setText(1, value['unit'])
-            item.setIcon(0, qta.icon(value['icon'], color=value['color']))
-
-        for key, value in EconomicStreams.items():
-            item = QtWidgets.QTreeWidgetItem(eco_root, [key, ''])
-            item.setText(0, key)
-            item.setText(1, value['unit'])
-            item.setIcon(0, qta.icon(value['icon'], color=value['color']))
-
-        # attach add button to root
-        self._attach_add_button(new_root)
+        # Attach button to `new_root`:
+        self._attach_add_button(self._new_root)
+        self._attach_add_button(self._fue_root)
+        self._attach_add_button(self._raw_root)
+        self._populate_default_streams()
 
     # Attach button to the `custom` branch:
     def _attach_add_button(self, parent_item: QtWidgets.QTreeWidgetItem) -> None:
@@ -150,50 +54,132 @@ class StreamConfigWidget(QtWidgets.QTreeWidget):
         toolbar.addWidget(expander)
         toolbar.setIconSize(QtCore.QSize(20, 20))
         toolbar.setStyleSheet('QToolBar {padding: 2px;} QToolBar QToolButton { margin: 0px; padding: 0px; }')
-        toolbar.addAction(qta.icon('mdi.plus', color='#ffcb00'), 'Add', lambda: self.define_new_composite(parent_item))
+
+        if  parent_item is self._raw_root:
+            toolbar.addAction(
+                qta.icon('mdi.plus', color='#ffcb00'), 'Add',
+                self._define_raw_material
+            )
+
+        if  parent_item is self._fue_root:
+            toolbar.addAction(
+                qta.icon('mdi.plus', color='#ffcb00'), 'Add',
+                self._define_fuel_type
+            )
+
+        if  parent_item is self._new_root:
+            toolbar.addAction(
+                qta.icon('mdi.plus', color='#ffcb00'), 'Add',
+                self._define_raw_material if parent_item == self._raw_root else self._define_new_resource
+            )
 
         # Place the widget into column 1 of the specified item
         self.setItemWidget(parent_item, 1, toolbar)
 
-    # Add a new custom item:
-    def define_new_composite(self, parent_item: QtWidgets.QTreeWidgetItem) -> None:
+    def _populate_default_streams(self) -> None:
+        """Create tree items for the default streams using their metadata."""
+        from apps.stream import default as streams
 
-        # Expand the `custom` branch so the new item is visible:
-        parent_item.setExpanded(True)
+        physical_keys = {"mass", "flow", "energy", "electricity"}
+        economic_keys = {"opex", "capex" , "revenue"}
+        emission_keys = {"GHG"  , "SOx", "NOx", "pm2.5", "pm10"}
 
-        # Create the new composite item and allow editing of its label:
-        new_item = QtWidgets.QTreeWidgetItem(parent_item, ["New Resource", ""])
-        new_item.setIcon(0, qta.icon('mdi.cube-outline', color='#ffcb00'))
-        new_item.setFlags(new_item.flags() | QtCore.Qt.ItemFlag.ItemIsEditable)
+        for cls in streams.DEFAULT_STREAMS.values():
+            meta = cls.metadata()
+            key = meta["key"]
+            label = meta.get("label", key.title())
+            unit = meta.get("unit", "")
+            icon_name = meta.get("icon", "")
+            color = meta.get("color", "")
 
-        # Build a small toolbar with checkable actions for each non-composite resource
+            # decide parent
+            if  key in physical_keys:
+                parent = self._phy_root
+            elif key in economic_keys:
+                parent = self._eco_root
+            elif key in emission_keys:
+                parent = self._ghg_root
+            else:
+                parent = self._new_root
+
+            item = QtWidgets.QTreeWidgetItem(parent)
+            item.setText(0, label)
+            item.setText(1, unit)
+            item.setIcon(0, qta.icon(icon_name, color=color))
+
+    # Method to compose a new stream selection item:
+    def _define_new_resource(self):
+
+        # Import default streams:
+        from apps.stream import default as streams
+
+        # Create a child under the "Custom" root
+        item = QtWidgets.QTreeWidgetItem(self._new_root)
+        item.setText(0, "New Resource")
+        item.setIcon(0, qta.icon('mdi.cube', color='#ffcb00'))
+        item.setFlags(item.flags() | QtCore.Qt.ItemFlag.ItemIsEditable)
+
+        # Create toolbar widget to hold icon actions
         toolbar = QtWidgets.QToolBar(self)
         toolbar.setIconSize(QtCore.QSize(18, 18))
-        toolbar.setStyleSheet('QToolBar {padding: 2px;} QToolBar QToolButton { margin: 0px; padding: 2px; }')
+        toolbar.setStyleSheet('QToolBar {padding: 2px;} QToolBar QToolButton { margin: 0px; padding: 0px; }')
 
-        m_tool = toolbar.addAction(qta.icon('mdi.weight-gram', color='lightblue'), 'Mass')
-        f_tool = toolbar.addAction(qta.icon('mdi.speedometer', color='#00aced'), 'Flow')
-        e_tool = toolbar.addAction(qta.icon('mdi.thermometer', color='#ffcb00'), 'Energy')
-        p_tool = toolbar.addAction(qta.icon('mdi.flash', color='#8c2b76'), 'Electricity')
-        c_tool = toolbar.addAction(qta.icon('mdi.alpha-c', color='#efefef'), 'Carbon')
-        s_tool = toolbar.addAction(qta.icon('mdi.alpha-s', color='#efefef'), 'Sulphur')
-        n_tool = toolbar.addAction(qta.icon('mdi.alpha-n', color='#efefef'), 'Nitrogen')
-        pms_tool = toolbar.addAction(qta.icon('mdi.circle-small', color='#efefef'), 'PM 2.5')
-        pmm_tool = toolbar.addAction(qta.icon('mdi.circle-medium', color='#efefef'), 'PM 10')
-        capex_tool = toolbar.addAction(qta.icon('mdi.cash', color='lightgreen'), 'CapEx')
-        oprex_tool = toolbar.addAction(qta.icon('mdi.account-cog' , color='red'), 'OprEx')
+        # Add one checkable action per default stream, store the stream key in action.data()
+        for cls in streams.DEFAULT_STREAMS.values():
 
-        m_tool.setCheckable(True)
-        f_tool.setCheckable(True)
-        e_tool.setCheckable(True)
-        p_tool.setCheckable(True)
-        c_tool.setCheckable(True)
-        s_tool.setCheckable(True)
-        n_tool.setCheckable(True)
-        pms_tool.setCheckable(True)
-        pmm_tool.setCheckable(True)
-        capex_tool.setCheckable(True)
-        oprex_tool.setCheckable(True)
+            meta = cls.metadata()
+            key  = meta.get("key")
+            name = meta.get("label", key.title())
+            icon = meta.get("icon", "")
+            color = meta.get("color", "")
+            action = toolbar.addAction(qta.icon(icon, color=color), name)
 
-        self.setItemWidget(new_item, 1, toolbar)
-        self.editItem(new_item, 0)
+            action.setToolTip(name)
+            action.setCheckable(True)
+            action.setData(key)
+
+        # Place the toolbar widget into column 1 of the newly created item
+        toolbar.addAction(qta.icon('mdi.close-circle', color='red'), 'Delete', )
+        self.setItemWidget(item, 1, toolbar)
+        self.editItem(item, 0)
+        self._new_root.setExpanded(True)
+
+    # Method to define a new fuel type:
+    def _define_fuel_type(self):
+
+        # Create a child under the "Fuel" root
+        item = QtWidgets.QTreeWidgetItem(self._fue_root)
+        item.setText(0, "New Fuel")
+        item.setIcon(0, qta.icon('mdi.fuel', color='#ffcb00'))
+        item.setFlags(item.flags() | QtCore.Qt.ItemFlag.ItemIsEditable)
+
+        # Create toolbar widget to hold icon actions
+        toolbar = QtWidgets.QToolBar(self)
+        toolbar.setIconSize(QtCore.QSize(18, 18))
+        toolbar.setStyleSheet('QToolBar {padding: 2px;} QToolBar QToolButton { margin: 0px; padding: 0px; }')
+
+        # Place the toolbar widget into column 1 of the newly created item
+        toolbar.addAction(qta.icon('mdi.close-circle', color='red'), 'Delete', )
+        self.setItemWidget(item, 1, toolbar)
+        self.editItem(item, 0)
+        self._fue_root.setExpanded(True)
+
+    # Method to define a new raw material:
+    def _define_raw_material(self):
+
+        # Create a child under the "Custom" root
+        item = QtWidgets.QTreeWidgetItem(self._raw_root)
+        item.setText(0, "New Resource")
+        item.setIcon(0, qta.icon('mdi.cube', color='#ffcb00'))
+        item.setFlags(item.flags() | QtCore.Qt.ItemFlag.ItemIsEditable)
+
+        # Create toolbar widget to hold icon actions
+        toolbar = QtWidgets.QToolBar(self)
+        toolbar.setIconSize(QtCore.QSize(18, 18))
+        toolbar.setStyleSheet('QToolBar {padding: 2px;} QToolBar QToolButton { margin: 0px; padding: 0px; }')
+
+        # Place the toolbar widget into column 1 of the newly created item
+        toolbar.addAction(qta.icon('mdi.close-circle', color='red'), 'Delete', )
+        self.setItemWidget(item, 1, toolbar)
+        self.editItem(item, 0)
+        self._raw_root.setExpanded(True)
